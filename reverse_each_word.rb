a2a70007b017d80711d4c38f592789897a525158
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 20:48:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   word_array = phrase.split
-  backword_phrase = []
+  backwards_phrase = []
   word_array.each {|word| backword_phrase << (word).reverse}
-  return backword_phrase.join 
+  return backwards_phrase.join 
 end
